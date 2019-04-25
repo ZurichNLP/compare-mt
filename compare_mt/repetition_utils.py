15775777e6a22cb63b669a45d4f06823260c6d45
Mathias Müller <mathias.mueller@uzh.ch>
2019-04-25 09:56:45 +0200

    repetition docs, tests

--- compare_mt/repetition_utils.py
+++ compare_mt/repetition_utils.py
@@ -1,7 +1,8 @@
 
 import itertools
+import numpy as np
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from collections import Counter
 
 from compare_mt import ngram_utils
@@ -13,74 +14,160 @@ Sentences = List[Tokens]
 
 def repetition_stats(ref: Sentences,
                      outs: List[Sentences],
-                     src: Sentences = None,
+                     src: Optional[Sentences] = None,
                      adjacent: bool = True,
                      ngram_order: int = 1,
-                     subtract_legitimate_reps: bool = False):
+                     subtract_legitimate_reps: bool = False) -> List[int]:
+    """
+    For each sentence translated by a system, collects the number of repeated elements.
+
+    :param ref: Lines from a reference corpus, in the target language.
+    :param outs: Lines from several system hypotheses.
+    :param src: Lines from a reference corpus, in the source language.
+    :param adjacent: Whether repeated elements need to occur adjacent
+                     to each other to count towards repetitions.
+    :param ngram_order: Order of ngrams considered, positive integer.
+    :param subtract_legitimate_reps: Whether to account for repetitions that are actually
+                                     present in the ref or src data.
+
+    :return: Lists of integers, where list elements correspond to lines in an input corpus.
     """
 
-    :param ref:
-    :param outs:
-    :param src:
-    :param adjacent:
-    :param ngram_order:
-    :param subtract_legitimate_reps:
-    :return:
+    rep_totals_per_out = []  # type: List[int]
+
+    for out in outs:
+        rep_total = repetition_stats_in_corpus(ref, out, src, adjacent,
+                                               ngram_order, subtract_legitimate_reps)
+        rep_totals_per_out.append(rep_total)
+
+    return rep_totals_per_out
+
+
+def repetition_stats_in_corpus(ref: Sentences,
+                                out: Sentences,
+                                src: Optional[Sentences] = None,
+                                adjacent: bool = True,
+                                ngram_order: int = 1,
+                                subtract_legitimate_reps: bool = False) -> List[int]:
+    """
+    For each sentence translated by a system, collects the number of repeated elements.
+
+    :param ref: Lines from a reference corpus, in the target language.
+    :param out: Translations from a single system.
+    :param src: Lines from a reference corpus, in the source language.
+    :param adjacent: Whether repeated elements need to occur adjacent
+                     to each other to count towards repetitions.
+    :param ngram_order: Order of ngrams considered, positive integer.
+    :param subtract_legitimate_reps: Whether to account for repetitions that are actually
+                                     present in the ref or src data.
+
+    :return: A list of integers, where list elements correspond to lines in the input corpus.
     """
 
-    reps_totals = []
+    reps_per_line = []
 
     if src is None:
         src_lines = []
 
-    for out_lines in outs:
-        reps = 0
+    for out_line, ref_line, src_line in itertools.zip_longest(out, ref, src_lines):
 
-        for out_line, ref_line, src_line in itertools.zip_longest(out_lines, ref, src_lines):
+        out_reps = num_repetitions_in_sentence(out_line, adjacent, ngram_order)
 
-            out_reps = num_repetitions_in_sentence(out_line, adjacent, ngram_order)
+        if subtract_legitimate_reps:
+            ref_reps = num_repetitions_in_sentence(ref_line, adjacent, ngram_order)
 
-            if subtract_legitimate_reps:
-                ref_reps = num_repetitions_in_sentence(ref_line, adjacent, ngram_order)
+            if src is None:
+                src_reps = 0
+            else:
+                src_reps = num_repetitions_in_sentence(src_line, adjacent, ngram_order)
 
-                if src is None:
-                    src_reps = 0
-                else:
-                    src_reps = num_repetitions_in_sentence(src_line, adjacent, ngram_order)
+            out_reps -= max(src_reps, ref_reps)
 
-                out_reps -= max(src_reps, ref_reps)
+        reps_per_line.append(out_reps)
 
-            reps += out_reps
-
-        reps_totals.append(reps)
-
-    return reps_totals
-
-
+    return reps_per_line
 
 def repetition_examples(ref: Sentences,
                         outs: List[Sentences],
-                        src: Sentences = None,
-                        report_length: int = 10):
+                        src: Optional[Sentences] = None,
+                        num_examples: int = 10,
+                        adjacent: bool = True,
+                        ngram_order: int = 1,
+                        ignore_legitimate_reps: bool = False
+                        ) -> List[Sentences]:
     """
+    Find the worst examples of repeated material in each set of system
+    translations (outs).
 
-    :param ref:
-    :param outs:
-    :param src:
-    :param report_length:
+    :param ref: Lines from a reference corpus, in the target language.
+    :param outs: Lines from several system hypotheses.
+    :param src: Lines from a reference corpus, in the source language.
+    :param num_examples: Number of example sentences to find.
+    :param adjacent: Whether repeated elements need to occur adjacent
+                     to each other to count towards repetitions.
+    :param ngram_order: Order of ngrams considered, positive integer.
+    :param ignore_legitimate_reps: Whether to account for repetitions that are actually
+                                   present in the ref or src data.
+
     :return:
     """
-    pass
+
+    examples_per_out = []
+
+    for out in outs:
+        examples = repetition_examples_from_corpus(out, ref, src, num_examples, adjacent,
+                                                   ngram_order, ignore_legitimate_reps)
+        examples_per_out.append(examples)
+
+    return examples_per_out
+
+def repetition_examples_from_corpus(out: Sentences,
+                                    ref: Sentences,
+                                    src: Optional[Sentences] = None,
+                                    num_examples: int = 10,
+                                    adjacent: bool = True,
+                                    ngram_order: int = 1,
+                                    ignore_legitimate_reps: bool = False
+                                    ) -> Sentences:
+    """
+    Find the worst examples of repeated material in a corpus (out).
+
+    :param out: Translations from a single system.
+    :param ref: Lines from a reference corpus, in the target language.
+    :param src: Lines from a reference corpus, in the source language.
+    :param num_examples: Number of example sentences to find.
+    :param adjacent: Whether repeated elements need to occur adjacent
+                     to each other to count towards repetitions.
+    :param ngram_order: Order of ngrams considered, positive integer.
+    :param ignore_legitimate_reps: Whether to account for repetitions that are actually
+                                   present in the ref or src data.
+
+    :return:
+    """
+
+    rep_stats = repetition_stats_in_corpus(ref, out, src, adjacent,
+                                            ngram_order, ignore_legitimate_reps)
+
+    # reverse sort index
+    sort_index = np.argsort(rep_stats, )[::-1]
+
+    trunc_index = sort_index[:num_examples]
+
+    worst_examples = [out[i] for i in trunc_index]
+
+    return worst_examples
 
 def num_repetitions_in_sentence(sentence: Tokens,
                                 adjacent: bool = True,
                                 ngram_order: int = 1) -> int:
     """
+    Counts repetitions in an input sentence.
 
     :param sentence: A list of tokens or characters as strings.
     :param adjacent: Whether repeated elements need to occur adjacent
                      to each other to count towards repetitions.
     :param ngram_order: Order of ngrams considered, positive integer.
+
     :return: Number of times an element was repeated.
     """
     num_repetitions = 0
@@ -111,11 +198,13 @@ def num_repetitions_in_sentence_pair(src_sentence: Tokens,
                                      ngram_order: int = 1) -> Tuple[int,int]:
     """
 
-    :param src_sentence:
-    :param trg_sentence:
-    :param adjacent:
-    :param ngram_order:
-    :return:
+    :param src_sentence: Line in source language.
+    :param trg_sentence: Line in target language.
+    :param adjacent: Whether repeated elements need to occur adjacent
+                     to each other to count towards repetitions.
+    :param ngram_order: Order of ngrams considered, positive integer.
+
+    :return: Number of repetitions in each sentence, as a tuple of integers.
     """
 
     src_reps = num_repetitions_in_sentence(src_sentence, adjacent, ngram_order)
--- tests/test_repetition_utils.py
+++ tests/test_repetition_utils.py
@@ -5,7 +5,7 @@ import sys
 compare_mt_root = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
 sys.path.append(compare_mt_root)
 
-from compare_mt.repetition_utils import num_repetitions_in_sentence
+from compare_mt.repetition_utils import num_repetitions_in_sentence, repetition_stats_in_corpus, repetition_examples_from_corpus
 
 
 class TestRepetitionUtils(unittest.TestCase):
@@ -61,3 +61,72 @@ class TestRepetitionUtils(unittest.TestCase):
                                                   ngram_order=2)
 
         self.assertEqual(expected_reps, actual_reps, "Wrong number of repetitions detected.")
+
+    def test_repetition_stats_in_corpus_unigram(self):
+
+        ref = ["This is the first test sentence".split(" "),
+               "This is the second test sentence".split(" ")]
+
+        out = ["This is is the first test sentence".split(" "),
+               "This is the the the the the second test is sentence".split(" ")]
+
+        expected = [1, 4]
+
+        actual = repetition_stats_in_corpus(ref=ref,
+                                            out=out,
+                                            src=None,
+                                            adjacent=True,
+                                            ngram_order=1,
+                                            subtract_legitimate_reps=False)
+
+        self.assertEqual(expected, actual, "Wrong number of repetitions detected.")
+
+        # also count non-adjacent repetitions
+
+        expected = [1, 5]
+
+        actual = repetition_stats_in_corpus(ref=ref,
+                                            out=out,
+                                            src=None,
+                                            adjacent=False,
+                                            ngram_order=1,
+                                            subtract_legitimate_reps=False)
+
+        self.assertEqual(expected, actual, "Wrong number of repetitions detected.")
+
+        # subtract legitimate reps in ref
+
+        ref_with_reps = ["This is is the first test sentence".split(" "),
+                         "This is the second test sentence".split(" ")]
+
+        expected = [0, 4]
+
+        actual = repetition_stats_in_corpus(ref=ref_with_reps,
+                                            out=out,
+                                            src=None,
+                                            adjacent=True,
+                                            ngram_order=1,
+                                            subtract_legitimate_reps=True)
+
+        self.assertEqual(expected, actual, "Wrong number of repetitions detected.")
+
+    def test_repetition_examples_from_corpus(self):
+        ref = ["This is the first test sentence".split(" "),
+               "This is the second test sentence".split(" "),
+               "This is the third test sentence".split(" ")]
+
+        out = ["This is is is is is is is the first test sentence".split(" "),
+               "This is the second test sentence".split(" "),
+               "This is the third third third test sentence".split(" ")]
+
+        expected = [out[0], out[2]]
+
+        actual = repetition_examples_from_corpus(out=out,
+                                                 ref=ref,
+                                                 src=None,
+                                                 num_examples=2,
+                                                 adjacent=True,
+                                                 ngram_order=1,
+                                                 ignore_legitimate_reps=False)
+
+        self.assertEqual(expected, actual, "Wrong examples from corpus.")
